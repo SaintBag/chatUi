6dfa294b1b384553c155209b022a944922863ca6
Sebastian Worek <sebastian1.worek@gmail.com>
2022-10-18 15:54:12 +0200

    Initial Commit

---
+++ chatUi/chatUiApp.swift
@@ -0,0 +1,20 @@
+//
+//  chatUiApp.swift
+//  chatUi
+//
+//  Created by Sebastian on 18/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct chatUiApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
