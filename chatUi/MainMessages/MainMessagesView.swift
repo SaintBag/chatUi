6310695bdf7f963a2a8918905cda9e61d381c245
Sebastian Worek <sebastian1.worek@gmail.com>
2022-10-20 10:47:35 +0200

    create Main view for messages

---
+++ chatUi/MainMessages/MainMessagesView.swift
@@ -0,0 +1,22 @@
+//
+//  MainMessagesView.swift
+//  chatUi
+//
+//  Created by Sebastian on 20/10/2022.
+//
+
+import SwiftUI
+
+struct MainMessagesView: View {
+    var body: some View {
+        NavigationView {
+            
+        }
+    }
+}
+
+struct MainMessagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainMessagesView()
+    }
+}
