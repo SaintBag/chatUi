979978881d88e68462cc665217ace811547a461d
Sebastian Worek <sebastian1.worek@gmail.com>
2022-10-27 13:52:03 +0200

    moving RecentMessage struct to Model

---
+++ Model/RecentMessage.swift
@@ -0,0 +1,33 @@
+//
+//  RecentMessage.swift
+//  chatUi
+//
+//  Created by Sebastian on 27/10/2022.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestore
+
+struct RecentMessage: Identifiable {
+    var id: String { documentId }
+    
+    let documentId: String
+    let text: String
+    let senderId: String
+    let reciverId: String
+    let email: String
+    let profileImageUrl: String
+    let timestamp: Timestamp
+    
+    init(documentId: String, data: [String: Any]) {
+        
+        self.documentId = documentId
+        self.text = data[FirebaseConstans.text] as? String ?? ""
+        self.senderId = data[FirebaseConstans.senderId] as? String ?? ""
+        self.reciverId = data[FirebaseConstans.reciverId] as? String ?? ""
+        self.email = data[FirebaseConstans.email] as? String ?? ""
+        self.profileImageUrl = data[FirebaseConstans.profileImageUrl] as? String ?? ""
+        self.timestamp = data[FirebaseConstans.timestamp] as? Timestamp ?? Timestamp(date: Date())
+    }
+}
--- chatUi/MainMessages/MainMessagesView.swift
+++ chatUi/MainMessages/MainMessagesView.swift
@@ -10,28 +10,7 @@ import SDWebImageSwiftUI
 import Firebase
 import FirebaseFirestore
 
-struct RecentMessage: Identifiable {
-    var id: String { documentId }
-    
-    let documentId: String
-    let text: String
-    let senderId: String
-    let reciverId: String
-    let email: String
-    let profileImageUrl: String
-    let timestamp: Timestamp
-    
-    init(documentId: String, data: [String: Any]) {
-        
-        self.documentId = documentId
-        self.text = data[FirebaseConstans.text] as? String ?? ""
-        self.senderId = data[FirebaseConstans.senderId] as? String ?? ""
-        self.reciverId = data[FirebaseConstans.reciverId] as? String ?? ""
-        self.email = data[FirebaseConstans.email] as? String ?? ""
-        self.profileImageUrl = data[FirebaseConstans.profileImageUrl] as? String ?? ""
-        self.timestamp = data[FirebaseConstans.timestamp] as? Timestamp ?? Timestamp(date: Date())
-    }
-}
+
 
 class MainMessageViewModel: ObservableObject {
     
@@ -178,7 +157,7 @@ struct MainMessagesView: View {
             ForEach(viewModel.recentMessages) { recentMessage in
                 VStack {
                     NavigationLink {
-                        Text("Destination")
+                        Text("Here new window schould show.")
                     } label: {
                         HStack(spacing: 16) {
 
